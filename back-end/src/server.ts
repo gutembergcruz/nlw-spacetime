7913bc63a929b64f8d45e4f551f71e6ee2e7235e
gutemberg234 <gute.tec@gmail.com>
2023-05-17 22:52:20 -0300

    memories routes

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,19 +1,19 @@
 import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
+import cors from "@fastify/cors";
+import { memoriesRoutes } from "./routes/memories";
 
 const app = fastify();
-const prisma = new PrismaClient();
 
-app.get("/users", async () => {
-  const users = await prisma.user.findMany();
-
-  return users;
+app.register(cors, {
+  origin: true,
 });
+
+app.register(memoriesRoutes);
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log("🚀 HTTP Server Running on http://localhost:3333");
+    console.log("🚀 HTTP server running on port http://localhost:3333");
   });
