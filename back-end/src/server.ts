5f79869255b17350daee331b34bee562127de4f2
gutemberg234 <gute.tec@gmail.com>
2023-05-19 02:52:00 -0300

    auth

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,6 +1,10 @@
+import "dotenv/config";
+
 import fastify from "fastify";
 import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
 import { memoriesRoutes } from "./routes/memories";
+import { authRoutes } from "./routes/auth";
 
 const app = fastify();
 
@@ -8,12 +12,18 @@ app.register(cors, {
   origin: true,
 });
 
+app.register(jwt, {
+  secret: "spacetime",
+});
+
+app.register(authRoutes);
 app.register(memoriesRoutes);
 
 app
   .listen({
     port: 3333,
+    host: "::",
   })
   .then(() => {
-    console.log("🚀 HTTP server running on port http://localhost:3333");
+    console.log("🚀 HTTP server running on http://localhost:3333");
   });
--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,18 +1,21 @@
-import { User } from "lucide-react";
+import { cookies } from "next/headers";
 
 import { Copyright } from "@/Components/Copyright";
 import { Hero } from "@/Components/Hero";
 import { SignIn } from "@/Components/SignIn";
 import { EmptyMemories } from "@/Components/EmptyMemories";
+import { Profile } from "@/Components/Profile";
 
 export default function Home() {
+  const isAuthenticated = cookies().has("token");
+
   return (
     <main className="grid min-h-screen grid-cols-2">
       {/* LEFT */}
       <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover p-16 px-28 py-16">
         <div className="absolute right-0 top-1/2 h-[288px] w-[256px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes"></div>
-        <SignIn />
+        {isAuthenticated ? <Profile /> : <SignIn />}
         <Hero />
         <Copyright />
       </div>
